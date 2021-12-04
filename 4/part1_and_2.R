d7ac1d7e3713d30329e04b357c5392a4b8f069a5
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-12-04 21:26:04 +0200

    read.delim simplifies reading a lot

--- 4/part1_and_2.R
+++ 4/part1_and_2.R
@@ -1,5 +1,3 @@
-library(comprehenr)
-
 # open the file
 connection = file("input", open="r")
 
@@ -7,60 +5,52 @@ connection = file("input", open="r")
 numbers = readLines(connection, n = 1)
 numbers = strsplit(numbers, split = ",")[[1]]
 
-# There is an empty line here
-readLines(connection, n = 1)
+# Read the rest and get the matrix
+tables = read.delim(connection, header=FALSE, sep="")
+tables = data.matrix(tables)
 
-tables = c()
+# the first index contains the rows of all the tables. Restructure here
+n_tables = dim(tables)[1]/5
+tables = array(tables, dim=c(5,n_tables,5))
+tables = aperm(tables, c(1,3,2))
 
-# Read the tables
-while ( TRUE ) {
-  
-  table = matrix(0, 5, 5)
-  for(i in 1:5){
-    line = readLines(connection, n = 1)
-    if(length(line) == 0){
-      break
-    }
-    line = trimws(line)
-    
-    split_line = strsplit(line, split = "\\s+")[[1]]
-    for(j in 1:5){
-      table[i,j] = strtoi(split_line[j]);
-    }
-  }
-  
-  if(length(line) == 0){
-    break
-  }
-  
-  tables = append(tables, list(table))
-  
-  # There is an empty line here
-  readLines(connection, n = 1)
-}
+# Now we have the tables as a 5x5x100 array
 
-marks = to_list(for(i in 1:length(tables)+1) matrix(0, 5, 5))
-winning_tables = matrix(FALSE, 1, length(tables))
+# Array to remember the marks
+marks_mat = array(0, dim=dim(tables))
+# Also remember which tables have already won and exclude them
+already_won = matrix(FALSE, 1, dim(tables)[3])
 
-# Now run through the list of numbers and check each table
+# Run through the number
+turn=0
 for(n in numbers){
-  for(t in 1:length(tables)){
-    if(winning_tables[t] == FALSE){
-      table = tables[[t]]
-      filter = ifelse( table == strtoi(n[[1]]), TRUE, FALSE)
-      mark = marks[[t]]
-      mark[filter] = 1
-      marks[[t]] = mark
-      
-      wins = sum(colSums(mark) == 5) + sum(rowSums(mark) == 5)
-      if(wins > 0){
-        filter = ifelse( mark == 0, TRUE, FALSE)
-        score = sum(table[filter])*strtoi(n[[1]])
-        print(sprintf("table %i wins with score %d", t, score))
-        winning_tables[t] = TRUE
-      }
+  # mark the number
+  filter = ifelse( tables == strtoi(n[[1]]), TRUE, FALSE)
+  marks_mat[filter] = 1
+  
+  # Check for fully marked rows and columns
+  row_win = apply(marks_mat, MARGIN=c(1, 3), sum) == 5
+  col_win = apply(marks_mat, MARGIN=c(2, 3), sum) == 5
+  win_count = apply(row_win + col_win, MARGIN=c(2), sum)
+  
+  winners = win_count>0 & !already_won
+  n_winners = sum(winners)
+
+  # mark winners so that they are excluded later
+  already_won[win_count>0] = TRUE
+  winning_indexes = which(winners)
+  
+  # calculate scores for each table
+  scores = tables
+  scores[marks_mat == 1] = 0
+  scores = apply(scores, MARGIN=c(3), sum)*strtoi(n[[1]])
+  scores = scores[winners]
+  
+  # print winners 
+  if(length(winning_indexes>0)){
+    for(i in 1:length(winning_indexes)){
+      print(sprintf("%d won on turn %d with score %d", winning_indexes[i], turn, scores[i]))
     }
   }
+  turn = turn+1
 }
-
-print(winning_tables[[length(winning_tables)]])
