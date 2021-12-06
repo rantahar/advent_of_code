6b488b25170aa86546825a8b8c4b267c5588dc2d
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-12-06 09:31:05 +0200

    day 6

---
+++ 6/part1_and_2.R
@@ -0,0 +1,25 @@
+library(gmp)
+
+# read the fish ages
+fish = scan("example_input", integer(), sep=",")
+
+DAYS = 256
+
+# count fish in each age group
+for(t in 1:9){
+  fish_by_age[t] = sum(fish==(t-1))
+}
+# Use big integers to avoid overflow
+fish_by_age = as.bigz(fish_by_age)
+
+# Run for given number of days
+for(d in 1:DAYS){
+  # Circular shift: update ages and add age 0 fish to age 8
+  fish_by_age = c(fish_by_age[2:9],fish_by_age[1])
+  
+  # The fish at age 0 are also added to age 6
+  fish_by_age[7] = fish_by_age[7] + fish_by_age[9]
+}
+
+print(sum(fish_by_age))
+
