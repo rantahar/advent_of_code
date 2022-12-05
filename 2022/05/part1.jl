c6aeb95f7583a2dc170e0ee1df4b8ccaa87a44f1
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-12-05 09:37:42 +0200

    Add day 5

---
+++ 2022/05/part1.jl
@@ -0,0 +1,39 @@
+using Formatting
+
+crates = []
+
+for line in readlines("input")
+    # Check if the line describes the starting stack
+    # and process it
+    if occursin("[", line)
+        for (i, crate) in enumerate(Iterators.partition(line, 4))
+            if crate[2] != ' '
+                while length(crates) < i
+                    append!(crates, [[]])
+                end
+                append!(crates[i], crate[2])
+            end
+        end
+    end
+
+    # Check for move commands and execute
+    if occursin("move", line)
+        words = split(line, " ")
+        count = parse(Int, words[2])
+        from = parse(Int, words[4])
+        to = parse(Int, words[6])
+
+        for i in 1:count
+            crate = popfirst!(crates[from])
+            pushfirst!(crates[to], crate)
+        end
+    end
+end
+
+print("Message: ")
+for stack in crates
+    print("", stack[1])
+end
+println()
+
+
---
+++ 2022/05/part2.jl
@@ -0,0 +1,38 @@
+using Formatting
+
+crates = []
+
+for line in readlines("input")
+    # Check if the line describes the starting stack
+    # and process it
+    if occursin("[", line)
+        for (i, crate) in enumerate(Iterators.partition(line, 4))
+            if crate[2] != ' '
+                while length(crates) < i
+                    append!(crates, [[]])
+                end
+                append!(crates[i], crate[2])
+            end
+        end
+    end
+
+    # Check for move commands and execute
+    if occursin("move", line)
+        words = split(line, " ")
+        count = parse(Int, words[2])
+        from = parse(Int, words[4])
+        to = parse(Int, words[6])
+
+        moved_crates = crates[from][1:count]
+        crates[from] = crates[from][count+1:end]
+        prepend!(crates[to], moved_crates)
+    end
+end
+
+print("Message: ")
+for stack in crates
+    print("", stack[1])
+end
+println()
+
+
