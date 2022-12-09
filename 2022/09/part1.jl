2be32ef378a8d3acc432303cbbddc80074db206b
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-12-09 21:11:46 +0200

    Day 9

---
+++ 2022/09/part1.jl
@@ -0,0 +1,54 @@
+
+lines = readlines("input")
+
+head = [1,1]
+tail = [1,1]
+touched = Set([tuple(1,1)])
+
+otherdir = [2,1]
+
+for line in lines
+    dir_label, num = split(line, " ")
+    num = parse(Int, num)
+    if dir_label == "R"
+        dir = 1
+        sign = 1
+    end
+    if dir_label == "L"
+        dir = 1
+        sign = -1
+    end
+    if dir_label == "U"
+        dir = 2
+        sign = 1
+    end
+    if dir_label == "D"
+        dir = 2
+        sign = -1
+    end
+    for step in 1:num
+        head[dir] += sign
+        for d in 1:2
+           if tail[d] + 1 < head[d]
+               tail[d] += 1
+               if tail[otherdir[d]] > head[otherdir[d]]
+                   tail[otherdir[d]] -= 1
+               elseif tail[otherdir[d]] < head[otherdir[d]]
+                       tail[otherdir[d]] += 1
+               end
+               break
+           elseif tail[d] - 1 > head[d]
+               tail[d] -= 1
+               if tail[otherdir[d]] > head[otherdir[d]]
+                   tail[otherdir[d]] -= 1
+               elseif tail[otherdir[d]] < head[otherdir[d]]
+                       tail[otherdir[d]] += 1
+               end
+               break
+           end
+        end
+        push!(touched, tuple(tail...))
+    end
+end
+
+println(length(touched))
---
+++ 2022/09/part2.jl
@@ -0,0 +1,59 @@
+
+lines = readlines("input")
+
+knots = []
+for k in 1:10
+    push!(knots, [1,1])
+end
+touched = Set([tuple(1,1)])
+
+otherdir = [2,1]
+
+for line in lines
+    dir_label, num = split(line, " ")
+    num = parse(Int, num)
+    if dir_label == "R"
+        dir = 1
+        sign = 1
+    end
+    if dir_label == "L"
+        dir = 1
+        sign = -1
+    end
+    if dir_label == "U"
+        dir = 2
+        sign = 1
+    end
+    if dir_label == "D"
+        dir = 2
+        sign = -1
+    end
+    for step in 1:num
+        knots[1][dir] += sign
+        for k in 2:length(knots)
+            for d in 1:2
+               if knots[k][d] + 1 < knots[k-1][d]
+                   knots[k][d] += 1
+                   if knots[k][otherdir[d]] > knots[k-1][otherdir[d]]
+                       knots[k][otherdir[d]] -= 1
+                   elseif knots[k][otherdir[d]] < knots[k-1][otherdir[d]]
+                           knots[k][otherdir[d]] += 1
+                   end
+                   break
+               elseif knots[k][d] - 1 > knots[k-1][d]
+                   knots[k][d] -= 1
+                   if knots[k][otherdir[d]] > knots[k-1][otherdir[d]]
+                       knots[k][otherdir[d]] -= 1
+                   elseif knots[k][otherdir[d]] < knots[k-1][otherdir[d]]
+                           knots[k][otherdir[d]] += 1
+                   end
+                   break
+               end
+            end
+        end
+        push!(touched, tuple(knots[end]...))
+
+    end
+end
+
+println(length(touched))
