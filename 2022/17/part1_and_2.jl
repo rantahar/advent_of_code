b72a9a8a36d367991eb4123e100001c24643a618
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-12-17 22:42:51 +0200

    flip indexes, small speedup

--- 2022/17/part1_and_2.jl
+++ 2022/17/part1_and_2.jl
@@ -1,10 +1,10 @@
-lines = readlines("input")
+lines = readlines("test_input")
 
 shapes = [
-    [1 1 1 1;],
-    [0 1 0 ; 1 1 1 ; 0 1 0],
-    [1 1 1 ; 0 0 1 ; 0 0 1],
     reshape([1 ; 1 ; 1 ; 1], :, 1), # force this to be 2D
+    [0 1 0 ; 1 1 1 ; 0 1 0],
+    [1 0 0 ; 1 0 0 ; 1 1 1],
+    [1 1 1 1;],
     [1 1 ; 1 1]
 ]
 
@@ -15,19 +15,19 @@ println(length(wind_pattern))
 
 function print_pattern(pattern, shape, x, y)
     pattern = copy(pattern)
-    for i in 1:size(shape)[1]
-        for j in 1:size(shape)[2]
-            if shape[i, j] == 1
-                pattern[y+i-1, x+j-1] = 2
+    for i in 1:size(shape)[2]
+        for j in 1:size(shape)[1]
+            if shape[j, i] == 1
+                pattern[x+j-1, y+i-1] = 2
             end
         end
     end
-    for i in 1:size(pattern)[1]
+    for i in 1:size(pattern)[2]
         print("|")
-        for j in 1:size(pattern)[2]
-            if pattern[size(pattern)[1]+1-i, j] == 1
+        for j in 1:size(pattern)[1]
+            if pattern[j, size(pattern)[2]+1-i] == 1
                 print("#")
-            elseif pattern[size(pattern)[1]+1-i, j] == 2
+            elseif pattern[j, size(pattern)[1]+1-i] == 2
                 print("@")
             else
                 print(".")
@@ -38,6 +38,19 @@ function print_pattern(pattern, shape, x, y)
     println()
 end
 
+
+function profile(rocks, height)
+    profile = fill(-1,7)
+    for x in 1:7
+        for h in 1:50
+            if rocks[x,height-h] == 1
+                profile[x] = h
+                break
+            end
+        end
+    end
+    return profile
+end
 
 function run(N, find_period = false)
     global shapes, wind_pattern
@@ -53,18 +66,18 @@ function run(N, find_period = false)
         x = 3
         y = height + 4
         shape = shapes[mod(s,length(shapes))+1]
-        if y + size(shape)[1] > size(rock_pattern)[2]
+        if y + size(shape)[2] > size(rock_pattern)[2]
             rock_pattern = hcat(rock_pattern, fill(0, size(rock_pattern)))
         end
         #print_pattern(rock_pattern, shape, x, y)
         while true
             w = wind_pattern[mod(wind, length(wind_pattern))+1]
             wind += 1
-            if 0 < x+w && x+w+size(shape)[2]-1 < 8
+            if 0 < x+w && x+w+size(shape)[1]-1 < 8
                 can_move = true
-                for xs in 1:size(shape)[2]
-                    for ys in 1:size(shape)[1]
-                        if shape[ys, xs] == 1 && rock_pattern[x+w+xs-1, y+ys-1] == 1
+                for xs in 1:size(shape)[1]
+                    for ys in 1:size(shape)[2]
+                        if shape[xs, ys] == 1 && rock_pattern[x+w+xs-1, y+ys-1] == 1
                             can_move = false
                             break
                         end
@@ -79,9 +92,9 @@ function run(N, find_period = false)
             end
             #print_pattern(rock_pattern, shape, x, y)
             touches = false
-            for xs in 1:size(shape)[2]
-                for ys in 1:size(shape)[1]
-                    if shape[ys, xs] == 1 && rock_pattern[x+xs-1, y+ys-2] == 1
+            for xs in 1:size(shape)[1]
+                for ys in 1:size(shape)[2]
+                    if shape[xs, ys] == 1 && rock_pattern[x+xs-1, y+ys-2] == 1
                         touches = true
                         break
                     end
@@ -91,14 +104,14 @@ function run(N, find_period = false)
                 end
             end
             if touches
-                for ys in 1:size(shape)[1]
-                    for xs in 1:size(shape)[2]
-                        if shape[ys, xs] == 1
+                for ys in 1:size(shape)[2]
+                    for xs in 1:size(shape)[1]
+                        if shape[xs, ys] == 1
                             rock_pattern[x+xs-1, y+ys-1] = 1
                         end
                     end
                 end
-                height = max(height, y + size(shape)[1] - 1)
+                height = max(height, y + size(shape)[2] - 1)
                 break
             else
                 y -= 1
@@ -108,11 +121,11 @@ function run(N, find_period = false)
 
         if find_period && mod(s, length(wind_pattern)*5) == 0
             if s == length(wind_pattern)*5
-                after_first_cycle = copy(rock_pattern[:, height-50:height])
+                after_first_cycle = profile(rock_pattern, height)
                 previous_height = height
             elseif s > 0
-                println("cycle ", s÷(length(wind_pattern)*5))
-                if rock_pattern[:, height-50:height] == after_first_cycle
+                #println("cycle ", s÷(length(wind_pattern)*5))
+                if profile(rock_pattern, height) == after_first_cycle
                     # pattern found
                     println("pattern_found at ", s, " height = ", height-1, ", diff = ", height-previous_height)
                     println("steps in cycle:", s-length(wind_pattern)*5)
